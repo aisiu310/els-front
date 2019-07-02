4b5058523edcd1bdc9407d05fb8ac0134285bf9f
cMbenny <distance00@126.com>
2019-07-02 16:20:46 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App1'
+import App from './App'
 import iView from 'iview'
-import router from './router/reuse'
+import router from './router'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
