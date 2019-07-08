29c6f9a2024a82e9d1b3ec309366142aabb1c7ad
Bierxiensi <2608473362@qq.com>
2019-07-08 19:55:56 +0800

    7-8-zxy

--- src/components/business_hall/common/Navigator.vue
+++ src/components/business_hall/common/Navigator.vue
@@ -70,87 +70,87 @@ export default {
 </script>
 
 <style>
-#main {
-  border: 4px solid red;
-  margin-left: 16%;
-  width: 84%;
-  padding: 1% 1% 0% 1%;
-}
-#main p {
-  font-size: 2em;
-}
-#main hr {
-  /*内嵌水平线*/
-  width: 100%;
-  margin: 0% 0% 1% 0%;
+  #main {
+    border: 4px solid red;
+    margin-left: 16%;
+    width: 84%;
+    padding: 1% 1% 0% 1%;
+  }
+  #main p {
+    font-size: 2em;
+  }
+  #main hr {
+    /*内嵌水平线*/
+    width: 100%;
+    margin: 0% 0% 1% 0%;
 
-  border-top: 3px solid rgba(0, 0, 0, 0.1);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-}
+    border-top: 3px solid rgba(0, 0, 0, 0.1);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  }
 
-#navigator {
-  /*width: 238px;
-		height: 780px;*/
+  #navigator {
+    /*width: 238px;
+      height: 780px;*/
 
-  width: 16%;
-  height: auto;
-  border: 1px solid red;
-  margin-top: 0px;
-  background-color: black;
+    width: 16%;
+    height: auto;
+    border: 1px solid red;
+    margin-top: 0px;
+    background-color: black;
 
-  float: left;
-}
-#navigator-bar {
-  padding: 6% 0% 0% 0%;
-  width: 100%;
-  border: 0px solid green;
-}
-#navigator-bar-item {
-  color: white;
-  font-size: 1.6rem;
-  text-align: center;
-}
-#navigator-bar-item li {
-  padding: 4% 0% 4% 0%;
-  /*border: solid 1px green;*/
-}
-#navigator-bar-item li:hover {
-  color: black;
-  background-color: white;
+    float: left;
+  }
+  #navigator-bar {
+    padding: 6% 0% 0% 0%;
+    width: 100%;
+    border: 0px solid green;
+  }
+  #navigator-bar-item {
+    color: white;
+    font-size: 1.6rem;
+    text-align: center;
+  }
+  #navigator-bar-item li {
+    padding: 4% 0% 4% 0%;
+    /*border: solid 1px green;*/
+  }
+  #navigator-bar-item li:hover {
+    color: black;
+    background-color: white;
 
-  -webkit-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  -ms-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  transition: all 0.4s;
-}
+    -webkit-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+    -ms-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    transition: all 0.4s;
+  }
 
-#employee-info {
-  width: 100%;
-  height: auto;
-  padding: 30% 0% 0% 0%;
+  #employee-info {
+    width: 100%;
+    height: auto;
+    padding: 30% 0% 0% 0%;
 
-  border: 0px solid blueviolet;
-}
-#employee-photo {
-  color: white;
+    border: 0px solid blueviolet;
+  }
+  #employee-photo {
+    color: white;
 
-  width: 100%;
-  height: auto;
-  text-align: center;
-  /*padding: 0% 25% 0% 25%;*/
+    width: 100%;
+    height: auto;
+    text-align: center;
+    /*padding: 0% 25% 0% 25%;*/
 
-  border: 0px solid red;
-}
-#employee-textinfo {
-  color: white;
-  text-align: center;
-  font-size: 20px;
-}
-#employee-textinfo ul {
-  padding: 0% 0% 0% 0%;
-}
-#employee-textinfo li {
-  padding: 2% 0% 4% 0%;
-}
+    border: 0px solid red;
+  }
+  #employee-textinfo {
+    color: white;
+    text-align: center;
+    font-size: 20px;
+  }
+  #employee-textinfo ul {
+    padding: 0% 0% 0% 0%;
+  }
+  #employee-textinfo li {
+    padding: 2% 0% 4% 0%;
+  }
 </style>
