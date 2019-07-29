f3950f639cf57e85c491d5fafe74575f04ebc858
cMbenny <distance00@126.com>
2019-07-29 10:59:45 +0800

    解决冲突

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -131,59 +131,6 @@ export default {
         self.$Message.error("用户名、密码、验证码不能为空");
       } else {
         if (self.formInline.authcode == self.checkCode) {
-<<<<<<< HEAD
-          self.$axios;
-          // .post("http://192.168.2.229:9001/yuantu/login_regist/login", {
-          //   account: self.formInline.user,
-          //   password: self.formInline.password
-          // })
-          // .then(response => {
-          //   console.log(response);
-          //   if (response.data.status === 200) {
-          //     self.$Message.success("登陆成功");
-          //  this.$Loading.finish();
-          //     self.$store.commit("setToken", response.data.data.userCode);
-          //     console.log(1);
-          //     self.$store.commit("setRole", response.data.data.identity);
-          //     console.log(2);
-          //     // self.$store.commit(
-          //     //   "setBusinessHallCode",
-          //     //   response.data.data.organizationCode
-          //     // );
-          //     sessionStorage.setItem(
-          //       "businessHallCode",
-          //       response.data.data.organizationCode
-          //     );
-
-          //     // self.$store.commit("setUserName", response.data.data.userName);
-          //     console.log(1, self.$store.state.login.role);
-          //     self.$router.push({
-          //       path: self.$store.state.login.role
-          //     });
-          //     if (response.data.data.identity === "businessHall") {
-          //       self.$store.commit(
-          //         "setBusinessHallCode",
-          //         response.data.data.identity
-          //       );
-          //     }
-          //   } else {
-          //     self.$Message.error(response.data.msg);
-          //   }
-          // })
-          // .catch(error => {
-          //   console.log(error);
-          //   self.$Message.error("服务器异常、检查连接信息");
-          // });
-          self.$store.commit("setToken", 123456);
-          self.$Message.success("登陆成功");
-          sessionStorage.setItem("hallCode", 18001);
-          sessionStorage.setItem("courierId", 18001123);
-          sessionStorage.setItem("courierName", "courier");
-          self.$store.commit("setRole", "businessHall");
-          this.$router.push({
-            path: self.$store.state.login.role
-          });
-=======
           self.$axios
             .post("http://192.168.2.229:9001/yuantu/login_regist/login", {
               account: self.formInline.user,
@@ -235,7 +182,6 @@ export default {
           // this.$router.push({
           //   path: self.$store.state.login.role
           // });
->>>>>>> b8497c2522e9755b35f4b67b7aaf6de20e044472
         } else {
           this.$Message.error("验证码错误！");
           this.$Loading.error();
