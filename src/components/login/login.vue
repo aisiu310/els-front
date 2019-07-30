5ae96c4015646fffec88d30f6674ffe1ee684655
cMbenny <distance00@126.com>
2019-07-30 18:01:25 +0800

    update router's bug

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -178,7 +178,7 @@ export default {
           sessionStorage.setItem("hallCode", 18001);
           sessionStorage.setItem("courierId", 18001123);
           sessionStorage.setItem("courierName", "courier");
-          self.$store.commit("setRole", "businessHall");
+          self.$store.commit("setRole", "admin");
           this.$router.push({
             path: self.$store.state.login.role
           });
