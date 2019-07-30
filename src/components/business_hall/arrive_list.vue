e2b36a79af89d44f9506d0c191e118b8e17c61fe
Bierxiensi <2608473362@qq.com>
2019-07-30 17:13:17 +0800

    zxy-7-30

--- src/components/business_hall/arrive_list.vue
+++ src/components/business_hall/arrive_list.vue
@@ -3,7 +3,6 @@
     <Tabs>
       <TabPane label="接收管理" icon="md-clipboard">
         <arriveListTable></arriveListTable>
-        <arriveListOption></arriveListOption>
       </TabPane>
 
       <TabPane label="历史记录" icon="ios-clock-outline">
@@ -14,12 +13,11 @@
 </template>
 <script>
 import arriveListTable from "./table/arrive_list_table";
-import arriveListOption from "./options/arrive_list_option";
 export default {
   components: {
-    arriveListTable,
-    arriveListOption
-  }
+    arriveListTable
+  },
+  methods: {}
 };
 </script>
 
